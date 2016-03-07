4b309abd67e75a2a619e1fa1def1436cb4ef28ff
snitelol <TomashenkoI@gmail.com>
2016-03-07 21:23:57 +0200

    Added files via upload

---
+++ Zadanie3.3/src/ua/goit/java/_.java
@@ -0,0 +1,14 @@
+package ua.goit.java;
+
+import java.util.List;
+
+/**
+ * Created by 111 on 07.03.2016.
+ */
+public class Музыкальный_магазин {
+    List<Гитара> Гитары;
+    List<Скрипка> Скрипки;
+    List<Труба> Трубы;
+    List<Ударные> Ударные;
+    List<Фортепиано> Фортепиано;
+}
